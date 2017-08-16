f671591bd168b861143ce9bc65277186f3ba3be9
Teamzone <teamzone@aboutagile.com>
2017-08-16 13:47:19 +0000

    It works

--- roman.js
+++ roman.js
@@ -4,15 +4,87 @@ function Roman() {
 
 Roman.prototype.convertToRoman = function (input) 
 {
-    if ((input == 1) || (input ==2)  || (input == 3))
-    {
-        var loop = input/1;
         var result = '';
-       for (i = 0; i < loop; i++) 
-       { 
-          result = result + 'I';
-       }
+     
+    while (input > 0)
+    {
+
+       if (900 <= input)
+        {
+            result = result + "CM";
+            input = input - 900;
+        }
+        
+     else if (500 <= input)
+        {
+            result = result + "D";
+            input = input - 500;
+        }
+             
+      else if (400 <= input)
+        {
+            result = result + "CD";
+            input = input - 400;
+        }
+                
+       else if (100 <= input)
+        {
+            result = result + "C";
+            input = input - 100;
+        }
+        
+       else if (90 <= input)
+        {
+            result = result + "XC";
+            input = input - 90;
+        }
+        
+       else if (50 <= input)
+        {
+            result = result + "L";
+            input = input - 50;
+        }
+        
+       else if (40 <= input)
+        {
+            result = result + "XL";
+            input = input - 40;
+        }
+        
+       else if (10 <= input)
+        {
+            result = result + "X";
+            input = input - 10;
+        }
+        
+       else if (9 <= input)
+        {
+            result = result + "IX";
+            input = input - 9;
+        }
+        
+        else if (5 <= input)
+        {
+            result = result + "V";
+            input = input - 5;
+        }
+        
+        else if (4 <= input)
+        {
+            result = result + "IV";
+            input = input - 4;
+        }
+        
+        else if (1 <= input)
+        {
+            result = result + "I";
+            input = input - 1;
+        }
+        
+
     }
+
+   return result;
 
  
  
--- romanTests.js
+++ romanTests.js
@@ -12,16 +12,79 @@ describe("Roman Numeral Tests", function () {
     });
     
       it("checkInput2", function(done) { 
+        assert(true, "test2");
         getRoman("II",2);
         done();
     });
     
-          it("checkInput3", function(done) { 
+    it("checkInput3", function(done) { 
         getRoman("III",3);
         done();
     });
     
+    it("checkInput4", function(done) { 
+        getRoman("IV",4);
+        done();
+    });
+        it("checkInput5", function(done) { 
+        getRoman("V",5);
+        done();
+    });
+        it("checkInput6", function(done) { 
+        getRoman("VI",6);
+        done();
+    });
+        it("checkInput9", function(done) { 
+        getRoman("IX",9);
+        done();
+    });
+        it("checkInput10", function(done) { 
+        getRoman("X",10);
+        done();
+    });
+        it("checkInput12", function(done) { 
+        getRoman("XII",12);
+        done();
+    });
+        it("checkInput42", function(done) { 
+        getRoman("XLII",42);
+        done();
+    });
     
+        it("checkInput55", function(done) { 
+        getRoman("LV",55);
+        done();
+    });
+    
+        it("checkInput93", function(done) { 
+        getRoman("XCIII",93);
+        done();
+    });
+    
+            it("checkInput415", function(done) { 
+        getRoman("CDXV",415);
+        done();
+    });    
+    
+            it("checkInput515", function(done) { 
+        getRoman("DXV",515);
+        done();
+    });  
+        
+        it("checkInput922", function(done) { 
+        getRoman("CMXXII",922);
+        done();
+    });  
+    
+        it("checkInput22", function(done) { 
+        getRoman("XXII",22);
+        done();
+    });  
+
+        it("checkInput200", function(done) { 
+        getRoman("CC",200);
+        done();
+    });  
       function getRoman(expected, input) {
         var instanceRoman = new Roman();
         var result = instanceRoman.convertToRoman(input);
